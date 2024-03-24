f9e553c41429398a9f32ae51a8251a164f126287
grmkris <kristjan.grm1@gmail.com>
2024-03-24 01:26:20 -0300

    create user example
    Signed-off-by: grmkris <kristjan.grm1@gmail.com>

--- app/[nickname]/page.tsx
+++ app/[nickname]/page.tsx
@@ -1,8 +1,23 @@
-'use client'
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
+import { eq } from "drizzle-orm";
+import {
+  FrameButton,
+  FrameContainer,
+  FrameImage,
+  FrameReducer,
+  getFrameMessage,
+  getPreviousFrame,
+  NextServerPageProps,
+  useFramesReducer,
+} from "frames.js/next/server";
+
+import { User, UserTable } from "../../schema";
+import { db } from "../api/user/route";
 import CustomLink, { CustomLinkProps } from "../components/CustomLink";
 import Footer from "../components/Footer";
 import { UserProfile } from "../components/UserProfile";
+import { createDebugUrl, DEFAULT_DEBUGGER_HUB_URL } from "../debug";
 
 export type UserType = {
   nickname: string;
@@ -34,8 +49,43 @@ const mockLinkData: CustomLinkProps[] = [
   },
 ];
 
-export default function UserDetail() {
-  const router = useRouter();
+type State = {};
+
+const initialState: State = {};
+
+const reducer: FrameReducer<State> = (state, action) => {
+  return {};
+};
+
+export default async function Home({ searchParams }: NextServerPageProps) {
+  const previousFrame = getPreviousFrame(searchParams);
+
+  const frameMessage = await getFrameMessage(previousFrame.postBody, {
+    hubHttpUrl: DEFAULT_DEBUGGER_HUB_URL,
+  });
+
+  if (frameMessage && !frameMessage?.isValid) {
+    throw new Error("Invalid frame payload");
+  }
+
+  const [state, dispatch] = useFramesReducer<State>(
+    reducer,
+    initialState,
+    previousFrame,
+  );
+
+  // Here: do a server side side effect either sync or async (using await), such as minting an NFT if you want.
+  // example: load the users credentials & check they have an NFT
+  console.log("info: state is:", state);
+
+  if (!searchParams?.nickname) throw new Error("No nickname provided");
+
+  const res = await db.query.UserTable.findFirst({
+    where: eq(UserTable.name, searchParams.nickname as string),
+    with: {
+      links: true,
+    },
+  });
 
   return (
     <div className="bg-zuriBg min-h-screen flex flex-col justify-start items-center gap-8 sm:gap-14 relative font-contact">
@@ -50,6 +100,52 @@ export default function UserDetail() {
         ))}
       </div>
       <Footer />
+      <Link href={createDebugUrl("/[nickname]/page")}>Debug</Link>
+      <FrameContainer
+        pathname="/"
+        postUrl="/frames"
+        state={state}
+        previousFrame={previousFrame}
+      >
+        <FrameImage>
+          {frameMessage ? (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              GM, {frameMessage.requesterUserData?.displayName}! Your FID is{" "}
+              {frameMessage.requesterFid}
+              {", "}
+              {frameMessage.requesterFid < 20_000
+                ? "you're OG!"
+                : "welcome to the Farcaster!"}
+            </div>
+          ) : (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              Say GM
+            </div>
+          )}
+        </FrameImage>
+        <FrameButton action={"link"} target={"/"}>
+          Say GM
+        </FrameButton>
+        <FrameButton action={"link"} target={"/"}>
+          Say GM
+        </FrameButton>
+        <FrameButton action={"link"} target={"/"}>
+          Say GM
+        </FrameButton>
+        <FrameButton action={"link"} target={"/"}>
+          Say GM
+        </FrameButton>
+      </FrameContainer>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -70,54 +70,6 @@ export default async function Home({ searchParams }: NextServerPageProps) {
       </Link>
       <ConnectWallet />
       <LinkManager />
-      <FrameContainer
-        postUrl="/frames"
-        pathname="/"
-        state={state}
-        previousFrame={previousFrame}
-      >
-        {/* <FrameImage src="https://framesjs.org/og.png" /> */}
-        <FrameImage aspectRatio="1.91:1">
-          <div tw="w-full h-full bg-slate-700 text-white justify-center items-center flex flex-col">
-            <div tw="flex flex-row">
-              {frameMessage?.inputText ? frameMessage.inputText : "Hello world"}
-            </div>
-            {frameMessage && (
-              <div tw="flex flex-col">
-                <div tw="flex">
-                  Requester is @{frameMessage.requesterUserData?.username}{" "}
-                </div>
-                <div tw="flex">
-                  Requester follows caster:{" "}
-                  {frameMessage.requesterFollowsCaster ? "true" : "false"}
-                </div>
-                <div tw="flex">
-                  Caster follows requester:{" "}
-                  {frameMessage.casterFollowsRequester ? "true" : "false"}
-                </div>
-                <div tw="flex">
-                  Requester liked cast:{" "}
-                  {frameMessage.likedCast ? "true" : "false"}
-                </div>
-                <div tw="flex">
-                  Requester recasted cast:{" "}
-                  {frameMessage.recastedCast ? "true" : "false"}
-                </div>
-              </div>
-            )}
-          </div>
-        </FrameImage>
-        <FrameInput text="put some text here" />
-        <FrameButton>
-          {state?.active === "1" ? "Active" : "Inactive"}
-        </FrameButton>
-        <FrameButton>
-          {state?.active === "2" ? "Active" : "Inactive"}
-        </FrameButton>
-        <FrameButton action="link" target={`https://www.google.com`}>
-          External
-        </FrameButton>
-      </FrameContainer>
     </div>
   );
 }
